c36f25e2832c39308039d063b09a1d5cd7fc2cad
Guillermo Montalban Martinez <guillermosysti@gmail.com>
2021-11-19 12:28:29 +0100

    creacion del proyecto y 3 visualizaciones

---
+++ GestionAmigos/GestionAmigos/GestionAmigosApp.swift
@@ -0,0 +1,17 @@
+//
+//  GestionAmigosApp.swift
+//  GestionAmigos
+//
+//  Created by Guillermo Montalban Martinez on 19/11/21.
+//
+
+import SwiftUI
+
+@main
+struct GestionAmigosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
